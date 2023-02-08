24580472a130982cefb03685701ca40913cd6d37
TarasPyts <taras.a.pyts@gmail.com>
2023-02-08 15:09:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { users } from './database/usersDB';
+import UserComponent from './components/UserComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {users.map((user, index) => {
+          let clsx = index % 2 ? 'even' : 'odd';
+          return <UserComponent item={user} cls={clsx} key={index} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/database/usersDB.js
@@ -0,0 +1,13 @@
+export let users = [
+  { name: 'vasyl', salery: 1000, age: 21, status: false },
+  { name: 'fdsyl', salery: 3000, age: 31, status: false },
+  { name: 'vfasdsyl', salery: 5000, age: 11, status: true },
+  { name: 'vafasdl', salery: 7000, age: 90, status: false },
+  { name: 'vafasdsyl', salery: 8000, age: 121, status: false },
+  { name: 'vfasyl', salery: 1000, age: 31321, status: true },
+  { name: 'vfasdl', salery: 2000, age: 3132, status: false },
+  { name: 'vafasdl', salery: 3000, age: 313, status: true },
+  { name: 'va12321syl', salery: 5000, age: 31, status: false },
+  { name: 'va3213syl', salery: 6000, age: 3231211, status: true },
+  { name: 'va3213syl', salery: 1500, age: 31321, status: true },
+];
